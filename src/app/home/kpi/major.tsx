8e79c05e15de41344f20f1684700df4229a8589a
Juhwan01 <wnghks5432@naver.com>
2024-01-25 21:38:01 +0900

    80으로 바꿈

--- src/app/home/kpi/major.tsx
+++ src/app/home/kpi/major.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function MajorKPI() {
-	return <h1 style={{ fontWeight: 'bold', fontSize: '4rem' }}>75%</h1>;
+	return <h1 style={{ fontWeight: 'bold', fontSize: '4rem' }}>80%</h1>;
 }
